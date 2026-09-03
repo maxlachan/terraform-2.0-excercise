f81867ee402c2b4378f3c860e43b71491eb365f5
steve1 <steve@example.com>
2026-09-03 11:58:10 +0530

    my first commit

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+var.ami = "ami-081b0a6eac00b4f53"
+var.instance_type = "t3.micro"
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "instance_type" {
+    type = string
+    description = "the instance type"
+}
+
+variable "ami"{
+    type = string
+    description = "the ami of the required instance "
+    
+}
